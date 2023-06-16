35723a7101e2f26e38b9bfa636c8653ddaf0c62c
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-16 13:55:38 +0530

    [PALLAVI] Add: File

---
+++ DataStructure-StackQueues/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DataStructure_StackQueues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StackQueueDemo stackLinkedList = new StackQueueDemo();
+            stackLinkedList.Push(70);
+            stackLinkedList.Push(30);
+            stackLinkedList.Push(56);
+            stackLinkedList.Display();
+        }
+    }
+    
+}
---
+++ DataStructure-StackQueues/StackQueueDemo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure_StackQueues
+{
+    internal class StackQueueDemo
+    {
+        private Node top;
+        public StackQueueDemo() //consturctor
+        {
+            this.top = null;
+        }
+        internal void Push(int Value)//Method to add elements in stack
+        {
+            Node node = new Node(Value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} Push to the stack", Value);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+
+        }
+    }
+    
+}
