5e20a07886c6a8c629804f12c0d326ccd6d24e37
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-17 08:41:07 +0530

    [PALLAVI] Add: PeekPopOperationTillStackEmpty

--- DataStructure-StackQueues/Program.cs
+++ DataStructure-StackQueues/Program.cs
@@ -4,11 +4,18 @@
     {
         static void Main(string[] args)
         {
-            StackQueueDemo stackLinkedList = new StackQueueDemo();
-            stackLinkedList.Push(70);
-            stackLinkedList.Push(30);
-            stackLinkedList.Push(56);
-            stackLinkedList.Display();
+            StackQueueDemo stackqueue = new StackQueueDemo();
+            stackqueue.Push(70);
+            stackqueue.Push(30);
+            stackqueue.Push(56);
+            Console.WriteLine("-----------Stack before empty operation-----------");
+            stackqueue.Display();
+            Console.WriteLine("\n--------------------------------------------------");
+            Console.Write("\n");
+            stackqueue.IsEmpty();
+            Console.WriteLine("-----------Stack after empty operation-----------");
+            stackqueue.Display();
+            Console.WriteLine("-------------------------------------------------");
         }
     }
     
--- DataStructure-StackQueues/StackQueueDemo.cs
+++ DataStructure-StackQueues/StackQueueDemo.cs
@@ -8,35 +8,67 @@ namespace DataStructure_StackQueues
 {
     internal class StackQueueDemo
     {
+
         private Node top;
-        public StackQueueDemo() //consturctor
+
+        public StackQueueDemo()
         {
             this.top = null;
         }
-        internal void Push(int Value)//Method to add elements in stack
+
+
+        internal void Push(int value)
         {
-            Node node = new Node(Value);
+            Node node = new Node(value);
             if (this.top == null)
-            {
                 node.next = null;
-            }
             else
-            {
                 node.next = this.top;
-            }
             this.top = node;
-            Console.WriteLine("{0} Push to the stack", Value);
+            Console.WriteLine("{0} pushed to stack ", value);
         }
+
+
         public void Display()
         {
             Node temp = this.top;
             while (temp != null)
             {
-                Console.WriteLine(temp.data + " ");
+                Console.Write(temp.data + " ");
                 temp = temp.next;
+            }
+        }
+
+        internal void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("{0} is in the top of the stack ", this.top.data);
+        }
+
+        internal void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty, Deletion is not possible");
+                return;
+            }
+            Console.WriteLine("Value popped is {0} ", this.top.data);
+            this.top = this.top.next;
+        }
+
+        internal void IsEmpty()
+        {
+            while (this.top != null)
+            {
+                Peek();
+                Pop();
             }
 
         }
+
     }
-    
 }
