65353c4fcc4a4303ff6ea0ab6efa5e2b0dd60ef2
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-23 17:24:57 +0530

    [PALLAVI] Add: Create Queue

--- DataStructure-StackQueues/Node.cs
+++ DataStructure-StackQueues/Node.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DataStructure_StackQueues
 {
-    internal class Node
+    public class Node
     {
         public int data;
         public Node next;
--- DataStructure-StackQueues/Program.cs
+++ DataStructure-StackQueues/Program.cs
@@ -4,18 +4,25 @@
     {
         static void Main(string[] args)
         {
-            StackQueueDemo stackqueue = new StackQueueDemo();
-            stackqueue.Push(70);
-            stackqueue.Push(30);
-            stackqueue.Push(56);
-            Console.WriteLine("-----------Stack before empty operation-----------");
-            stackqueue.Display();
-            Console.WriteLine("\n--------------------------------------------------");
-            Console.Write("\n");
-            stackqueue.IsEmpty();
-            Console.WriteLine("-----------Stack after empty operation-----------");
-            stackqueue.Display();
-            Console.WriteLine("-------------------------------------------------");
+            //StackQueueDemo stackqueue = new StackQueueDemo();
+            //stackqueue.Push(70);
+            //stackqueue.Push(30);
+            //stackqueue.Push(56);
+            //Console.WriteLine("-----------Stack before empty operation-----------");
+            //stackqueue.Display();
+            //Console.WriteLine("\n--------------------------------------------------");
+            //Console.Write("\n");
+            //stackqueue.IsEmpty();
+            //Console.WriteLine("-----------Stack after empty operation-----------");
+            //stackqueue.Display();
+            //Console.WriteLine("-------------------------------------------------");
+
+
+            Queue linkedListQueue = new Queue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            linkedListQueue.Display();
         }
     }
     
